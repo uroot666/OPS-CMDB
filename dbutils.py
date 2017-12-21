dcdac5ac9f4404f6011d3866d3a2e71af5091227
liangbiao666@gmail.com <liangbiao666@gmail.com>
2017-12-21 23:49:40 +0800

    修改异常处理

--- dbutils.py
+++ dbutils.py
@@ -4,14 +4,18 @@ import pymysql
 import config
 
 def db_operating(sql, if_fach,args=()):
-    rt_cnt, rt_list = 0, []
-    db = pymysql.connect(**config.config)
-    cursor = db.cursor()
-    rt_cnt = cursor.execute(sql, args)
-    if if_fach:
-        rt_list = cursor.fetchall()
-    else:
-        db.commit()
-    cursor.close()
-    db.close()
+    try:
+        rt_cnt, rt_list = 0, []
+        db = pymysql.connect(**config.config)
+        cursor = db.cursor()
+        rt_cnt = cursor.execute(sql, args)
+        if if_fach:
+            rt_list = cursor.fetchall()
+        else:
+            db.commit()
+    except BaseException as e:
+        print('db error')
+    finally:
+        cursor.close()
+        db.close()
     return rt_cnt,rt_list
